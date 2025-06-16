a61b82f9c8c721ca20e4ad52ebbad4f262a79c07
pkasemsu <nongk977@gmail.com>
2025-06-16 09:04:01 +0000

    name.py

---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+Last_name = "Kasemsuk"
+First_name = "Piriya"
+whole_name = Last_name + "  " +First_name
+print(whole_name)
---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,5 @@
+Number = float (input("Enter a number: ")) 
+if Number == 0:
+    print ("This number equal to zero")
+else:
+    print("This number is different from zero")
